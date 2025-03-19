1c927fda275c62ee2439f14bb5e98c2eec261704
ErdemCanK <95128215+ErdemCanK@users.noreply.github.com>
2025-03-19 03:59:30 +0300

    Enhance homepage and layout with new components and styling
    - Updated homepage to include logo, resume preview, and a call-to-action button
    - Added PremiumModal to the layout for premium features
    - Replaced button on resumes page with CreateResumeButton for better functionality
    - Introduced new premium button variant for enhanced styling

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,11 @@
 import Navbar from "./navbar";
-
+import PremiumModal from "@/components/premium/PremiumModal";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
                 {children}
-            
+                <PremiumModal />
         </div>
     )
 }
--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { PlusSquare } from "lucide-react";
-import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 import { resumeDataInclude } from "@/lib/types";
 import ResumeItem from "./ResumeItem";
+import CreateResumeButton from "./CreateResumeButton";
 export const metadata = {
   title: "Your Resumes",
   description: "Here are will be a list of resumes",
@@ -37,12 +35,7 @@ export default async function Page() {
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          New Resume
-        </Link>
-      </Button>
+     <CreateResumeButton canCreate={totalCount < 3} />
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your Resumes</h1>
         <p className="text-sm text-muted-foreground">
